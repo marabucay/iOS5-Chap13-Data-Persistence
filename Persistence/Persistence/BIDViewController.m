8473c5d56e884a316d6960e2b49e379a2607c205
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-19 14:17:38 +0800

    Changed data persistence technique to SQLite3.

--- Persistence/Persistence/BIDViewController.m
+++ Persistence/Persistence/BIDViewController.m
@@ -7,10 +7,9 @@
 //
 
 #import "BIDViewController.h"
-#import "BIDFourLines.h"
+#import <sqlite3.h>
 
-#define kFilename @"archive"
-#define kDataKey @"Data"
+#define kFilename @"data.sqlite3"
 
 @implementation BIDViewController
 @synthesize field1;
@@ -36,18 +35,37 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    NSString *filePath = [self dataFilePath];
-    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
-        NSData *data = [[NSMutableData alloc] initWithContentsOfFile:[self dataFilePath]];
-        NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc]initForReadingWithData:data];
-        BIDFourLines *fourLines = [unarchiver decodeObjectForKey:kDataKey];
-        [unarchiver finishDecoding];
-        field1.text = fourLines.field1;
-        field2.text = fourLines.field2;
-        field3.text = fourLines.field3;
-        field4.text = fourLines.field4;
+	sqlite3 *database;
+    if (sqlite3_open([[self dataFilePath] UTF8String], &database)
+        != SQLITE_OK) {
+        sqlite3_close(database);
+        NSAssert(0, @"Failed to open database");
     }
+    NSString *createSQL = @"CREATE TABLE IF NOT EXISTS FIELDS "
+    "(ROW INTEGER PRIMARY KEY, FIELD_DATA TEXT);";
+    char *errorMsg;
+    if (sqlite3_exec (database, [createSQL UTF8String],
+                      NULL, NULL, &errorMsg) != SQLITE_OK) {
+        sqlite3_close(database);
+        NSAssert(0, @"Error creating table: %s", errorMsg);
+    }
+    NSString *query = @"SELECT ROW, FIELD_DATA FROM FIELDS ORDER BY ROW";
+    sqlite3_stmt *statement;
+    if (sqlite3_prepare_v2(database, [query UTF8String],
+                           -1, &statement, nil) == SQLITE_OK) {
+        while (sqlite3_step(statement) == SQLITE_ROW) {
+            int row = sqlite3_column_int(statement, 0);
+            char *rowData = (char *)sqlite3_column_text(statement, 1);
+            NSString *fieldName = [[NSString alloc]
+                                   initWithFormat:@"field%d", row];
+            NSString *fieldValue = [[NSString alloc]
+                                    initWithUTF8String:rowData];
+            UITextField *field = [self valueForKey:fieldName];
+            field.text = fieldValue;
+        }
+        sqlite3_finalize(statement);
+    }
+    sqlite3_close(database);
     UIApplication *app = [UIApplication sharedApplication];
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(applicationWillResignActive:)
@@ -67,17 +85,31 @@
     self.field4 = nil;
 }
 - (void)applicationWillResignActive:(NSNotification *)notification {
-    BIDFourLines *fourLines = [[BIDFourLines alloc] init];
-    fourLines.field1 = field1.text;
-    fourLines.field2 = field2.text;
-    fourLines.field3 = field3.text;
-    fourLines.field4 = field4.text;
-    NSMutableData *data = [[NSMutableData alloc] init];
-    NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc]
-                                 initForWritingWithMutableData:data];
-    [archiver encodeObject:fourLines forKey:kDataKey];
-    [archiver finishEncoding];
-    [data writeToFile:[self dataFilePath] atomically:YES];
+    sqlite3 *database;
+    if (sqlite3_open([[self dataFilePath] UTF8String], &database)
+        != SQLITE_OK) {
+        sqlite3_close(database);
+        NSAssert(0, @"Failed to open database");
+    }
+    for (int i = 1; i <= 4; i++) {
+        NSString *fieldName = [[NSString alloc]
+                               initWithFormat:@"field%d", i];
+        UITextField *field = [self valueForKey:fieldName];
+        // Once again, inline string concatenation to the rescue:
+        char *update = "INSERT OR REPLACE INTO FIELDS (ROW, FIELD_DATA) "
+        "VALUES (?, ?);";
+        char *errorMsg;
+        sqlite3_stmt *stmt;
+        if (sqlite3_prepare_v2(database, update, -1, &stmt, nil)
+            == SQLITE_OK) {
+            sqlite3_bind_int(stmt, 1, i);
+            sqlite3_bind_text(stmt, 2, [field.text UTF8String], -1, NULL);
+        }
+        if (sqlite3_step(stmt) != SQLITE_DONE)
+            NSAssert(0, @"Error updating table: %s", errorMsg);
+        sqlite3_finalize(stmt);
+    }
+    sqlite3_close(database);
 }
 
 - (void)viewWillAppear:(BOOL)animated
