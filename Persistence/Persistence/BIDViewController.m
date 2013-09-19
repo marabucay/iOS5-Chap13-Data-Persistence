a2fd5823bac3ce51f28568cfa812604c8107a34d
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-19 13:19:06 +0800

    Edited the persistence classes.

--- Persistence/Persistence/BIDViewController.m
+++ Persistence/Persistence/BIDViewController.m
@@ -8,7 +8,20 @@
 
 #import "BIDViewController.h"
 
+#define kFilename @"data.plist"
+
 @implementation BIDViewController
+@synthesize field1;
+@synthesize field2;
+@synthesize field3;
+@synthesize field4;
+
+- (NSString *)dataFilePath {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(
+                                                         NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    return [documentsDirectory stringByAppendingPathComponent:kFilename];
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -22,13 +35,39 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSString *filePath = [self dataFilePath];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+        NSArray *array = [[NSArray alloc] initWithContentsOfFile:filePath];
+        field1.text = [array objectAtIndex:0];
+        field2.text = [array objectAtIndex:1];
+        field3.text = [array objectAtIndex:2];
+        field4.text = [array objectAtIndex:3];
+    }
+    UIApplication *app = [UIApplication sharedApplication];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillResignActive:)
+                                                 name:UIApplicationWillResignActiveNotification
+                                               object:app];
 }
+
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.field1 = nil;
+    self.field2 = nil;
+    self.field3 = nil;
+    self.field4 = nil;
+}
+- (void)applicationWillResignActive:(NSNotification *)notification {
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    [array addObject:field1.text];
+    [array addObject:field2.text];
+    [array addObject:field3.text];
+    [array addObject:field4.text];
+    [array writeToFile:[self dataFilePath] atomically:YES];
 }
 
 - (void)viewWillAppear:(BOOL)animated
