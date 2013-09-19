c946f750a39f4ef5a0ca88be32e72c802fdaa0bc
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-19 14:52:28 +0800

    Created the Persistence View and Controller.

---
+++ Core Data Persistence/Core Data Persistence/BIDViewController.h
@@ -0,0 +1,17 @@
+//
+//  BIDViewController.h
+//  Core Data Persistence
+//
+//  Created by Maria Alice C. Lim on 9/19/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *line1;
+@property (weak, nonatomic) IBOutlet UITextField *line2;
+@property (weak, nonatomic) IBOutlet UITextField *line3;
+@property (weak, nonatomic) IBOutlet UITextField *line4;
+
+@end
